205ad0f70869146ba6f57cc9d06cb7a277090910
Ettie1 <efverwant@gmail.com>
2025-06-28 15:07:09 +0200

    Updated DateTime issue

--- routes/tsRoute.js
+++ routes/tsRoute.js
@@ -2,24 +2,28 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res)=>{
-    let currentYear = 2025;
-    res.render('index', {title: "TECHIESpirals Digital",  currentYear: "2025", metaDescription: "defined"})
+    let currentYear = Date.now(s=> new Date().getFullYear());
+    res.render('index', {title: "TECHIESpirals Digital",  currentYear: currentYear, metaDescription: "defined"})
 })
 
 router.get('/about', (req, res)=>{
-    res.render('about', {title: "TECHIESpirals Digital",  currentYear: "2025", metaDescription: "defined"})
+    let currentYear = Date.now(s=> new Date().getFullYear());
+    res.render('about', { title: "TECHIESpirals Digital",  currentYear: currentYear, metaDescription: "defined"})
 })
 
 router.get('/contact', (req, res)=>{
-    res.render('contact', {title: "TECHIESpirals Digital",  currentYear: "2025", metaDescription: "defined", submitted: false})
+    let currentYear = Date.now(s=> new Date().getFullYear());
+    res.render('contact', {title: "TECHIESpirals Digital",  currentYear: currentYear, metaDescription: "defined", submitted: false})
 })
 
 router.get('/services', (req, res)=>{
-    res.render('services', {title: "TECHIESpirals Digital",  currentYear: "2025", metaDescription: "defined"})
+    let currentYear = Date.now(s=> new Date().getFullYear());
+    res.render('services', {title: "TECHIESpirals Digital",  currentYear: currentYear, metaDescription: "defined"})
 })
 
 router.get('/header', (req, res)=>{
-    res.render('./partials/header', {title: "TECHIESpirals Digital",  currentYear: "2025", metaDescription: "defined"})
+    let currentYear = Date.now(s=> new Date().getFullYear());
+    res.render('./partials/header', {title: "TECHIESpirals Digital",  currentYear: currentYear, metaDescription: "defined"})
 })
 
 // router.get('/services', (req, res)=>{
